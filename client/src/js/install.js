d0e13478276d66609ae2a8e2276b54362de25cdb
Matthew C <cnymhw86@gmail.com>
2023-03-17 09:05:43 -0400

    altered apphandler in install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,14 +3,11 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  butInstall.style.visibility = "visibility";
+  // Store the triggered events
+  window.deferredPrompt = event;
 
-  butInstall.addEventListener("click", () => {
-    event.prompt();
-    butInstall.setAttribute("disabled, true");
-    butInstall.textContent = "Installed!";
-  });
+  // Remove the hidden class from the button.
+  butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -27,6 +24,6 @@ butInstall.addEventListener("click", async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  textHeader.textContent = "Successfully installed!";
   console.log("👍", "appinstalled", event);
+  window.deferredPrompt = null;
 });
